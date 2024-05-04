ecd70e2bb092374803dfb8f6e74384d3d7e48861
Aitana <121254480+aitanarules@users.noreply.github.com>
2024-05-04 18:27:55 +0200

    Update lectura_serial.py

--- lectura_serial.py
+++ lectura_serial.py
@@ -1,15 +1,16 @@
 import serial
 import time
 
-filename = "./nombre_de_archivo_a_guardar.txt"
+filename = "./brightness.txt"
 
-# Asegúrate de usar el puerto y baudrate correctos
-ser = serial.Serial('COM15', 115200) 
-time.sleep(2) # Espera para la conexión serial
+# serial port name , fastness in baudios
+serial = serial.Serial('COM15', 115200) 
+# wait for the serial connection
+time.sleep(2)
 
 with open(filename , "w") as file:
     while True:
-        if ser.in_waiting:
-            line = ser.readline().decode('utf-8').rstrip()
+        if serial.in_waiting:
+            line = serial.readline().decode('utf-8').rstrip()
             print(line)
             file.write(line + "\n")
