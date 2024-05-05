006e9ab812ad5dbb04557ab24c47cc42d47cc0f3
Aitana <121254480+aitanarules@users.noreply.github.com>
2024-05-05 12:38:39 +0200

    added new arduino file

---
+++ esp32_hackupv.ino
@@ -0,0 +1,35 @@
+#include "HardwareSerial.h"
+#include "DHT.h"
+
+#define LIGHT_SENSOR_PIN 27 // ESP32 pin GIOP27 (ADC17)
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+HardwareSerial USBserial(0);
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  USBserial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  // 1. Tomamos luminosidad
+  int analogValue = analogRead(LIGHT_SENSOR_PIN);
+  USBserial.println(analogValue);
+  delay(500);
+
+  // 2. Tomamos temperatura y humedad
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  if (isnan(h) || isnan(t)) {
+    t = 0;
+    h = 0;
+    return;
+  }
+
+  USBserial.println(t);
+  USBserial.println(h);
+}
+
